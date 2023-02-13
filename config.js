603003eeeecf7fcff81e0ab199cf8ebc5a2f1c59
ꪶᴛᴏxɪᴄ-ʟᴇᴏꫂ⁩⁩⁩ <110882648+Afx-ToxicLeo@users.noreply.github.com>
2023-02-13 07:36:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || "M1dORm12UFM=",
+  SESSION_ID:process.env.SESSION_ID || "inrl~t8pRMxc1iegUGgR1LApync5hBy6/iLE61Xtk6w",
   LANG: process.env.LANG || "EN",
   HANDLERS:
     process.env.HANDLER === "false" || process.env.HANDLER === "null"

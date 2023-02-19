5f6b719f83a4d114b76c79502ac524182e156f6f
ꪶᴛᴏxɪᴄ-ʟᴇᴏꫂ⁩⁩⁩ <110882648+Afx-ToxicLeo@users.noreply.github.com>
2023-02-19 11:24:43 +0530

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -1,8 +1,3 @@
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
 
 const {
   Function,
@@ -45,11 +40,7 @@ const {
 const util = require("util");
 const config = require("../config");
 
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
+
 
 command({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
   if (match.startsWith(">")) {

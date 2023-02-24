cf6f00f76699150a60079237bbb775710324f57c
ꪶᴛᴏxɪᴄ-ʟᴇᴏꫂ⁩⁩⁩ <110882648+Afx-ToxicLeo@users.noreply.github.com>
2023-02-24 20:09:47 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -3,17 +3,16 @@ const { command } = require("../lib");
 
 command(
   {
-    pattern: "setpp ",
+    pattern: "setpp",
     fromMe: true,
-    desc: "Set profile picture",
     type: "user",
   },
   async (message, match, m) => {
     if (!message.reply_message.image)
-      return await message.reply("_Reply to a photo_");
+      return await message.reply(" *Please Reply To A Photo*");
     let buff = await m.quoted.download();
     await message.setPP(message.user, buff);
-    return await message.reply("_Profile Picture Updated_");
+    return await message.reply(" *Profile Picture Updated*");
   }
 );
 

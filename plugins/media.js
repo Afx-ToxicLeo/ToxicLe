226e1cdb84044218b8aed8a97e83fd887366f6b4
ꪶᴛᴏxɪᴄ-ʟᴇᴏꫂ⁩⁩⁩ <110882648+Afx-ToxicLeo@users.noreply.github.com>
2023-04-02 11:06:03 +0530

    Update media.js

--- plugins/media.js
+++ plugins/media.js
@@ -107,50 +107,7 @@ command(
 
 
 
-command(
-  {
-    pattern: "song",
-    fromMe: isPrivate,
-    desc: "Downloads Song",
-    type: "downloader",
-  },
-  async (message, match) => {
-    if (!(match || message.reply_message.text))
-      return await message.reply(" *Enter Song Name*");
-    match = match || message.reply_message.text;
-    if (ytIdRegex.test(match)) {
-      yta(match.trim()).then(async ({ dl_link, title, thumb }) => {
-        message.reply("1 " + dl_link)
-      let buff = await AddMp3Meta(dl_link, thumb, {
-          title,
-        });
-        message.sendMessage(
-          buff,
-          { mimetype: "audio/mpeg", quoted: message.data },
-          "audio"
-        );
-      });
-    }
-    search(match + "song").then(async ({ videos }) => {
-      await message.reply(` *Downloading ${videos[0].title}*`);
-      let a = videos[0].url
-    //  let b = a.url
-      await message.reply(`${a}`)
-      yta(videos[0].url).then(async ({ dl_link, title, thumb }) => {
-   //     message.reply()
-        let buff = await AddMp3Meta(dl_link, thumb, {
-          title,
-          artist: [videos[0].author],
-        });
-        message.sendMessage(
-          buff,
-          { mimetype: "audio/mpeg", quoted: message.data },
-          "audio"
-        );
-      });
-    });
-  }
-);
+
 
 
 

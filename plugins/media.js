1d32ad7d301881d6f3976d0934a4410a63e48710
ꪶᴛᴏxɪᴄ-ʟᴇᴏꫂ⁩⁩⁩ <110882648+Afx-ToxicLeo@users.noreply.github.com>
2023-04-02 11:09:24 +0530

    Update media.js

--- plugins/media.js
+++ plugins/media.js
@@ -86,6 +86,47 @@ command(
 );
 
 
+command(
+  {
+    pattern: "song",
+    fromMe: isPrivate,
+    type: "downloader",
+  },
+  async (message, match) => {
+    await message.reply("*Downloading your song...*");
+   await message.reply("*Uploading Your Song...*");
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply("_Enter Song Name_");
+    //fn
+    const dMp3 = async (Link ) => {
+    	try{
+    		await ytdl.getInfo(Link);
+    		let mp3File = getRandom('.mp3') 
+    		ytdl(Link, {filter: 'audioonly'})
+    		.pipe(fs.createWriteStream(mp3File))
+    		.on("finish", async () => {  
+    			await message.sendMessage(
+          fs.readFileSync(mp3File),
+          { mimetype: "audio/mpeg", quoted: message },
+          "audio"
+        );
+        fs.unlinkSync(mp3File)
+        })       
+        } catch (err){
+//console.log(err)
+}
+}
+var songId = await mYtId(match)
+if (songId !== null){
+	let Link = 'https://youtu.be/' + songId[1]
+	dMp3(Link)
+	} else {
+		let search = await yts(match)  
+		dMp3(search.all[0].url)
+	}
+}
+);
+
 
 command(
   {
